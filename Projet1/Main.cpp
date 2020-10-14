7bc53e5e9e63d0131e0ea2d76014afbba7e32558
Vincent Morin <VincentMorin@cegepjonquiere.ca>
2020-10-14 09:06:56 -0400

    ajout nouveau fichier et modif de commentaire dans le main

--- Projet1/Main.cpp
+++ Projet1/Main.cpp
@@ -4,6 +4,13 @@ using namespace sf;
 
 int main()
 {
+
+	/*Noms des étudiants
+	
+	1.Vincent Morin
+	
+	*/
+
 	std::string gameName = "MyGame";
 	GameView::Game myGame(800, 600, gameName);
 	myGame.init();
