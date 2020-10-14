5a3bb77ba6cf4c7f874b872f1b60859db54a2831
Tanini2 <31666041+Tanini2@users.noreply.github.com>
2020-10-14 09:41:52 -0400

    Correction dernier commit - Enlever HEAD

--- Projet1/Main.cpp
+++ Projet1/Main.cpp
@@ -4,17 +4,11 @@ using namespace sf;
 
 int main()
 {
-<<<<<<< HEAD
-
 	/*Noms des étudiants
 	
 	1.Vincent Morin
 	2 - Tanya
 	*/
-
-=======
-	
->>>>>>> Tanya
 	std::string gameName = "MyGame";
 	GameView::Game myGame(800, 600, gameName);
 	myGame.init();
