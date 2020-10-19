622a820cc5e43e8abf314dd4c187b7adcc365475
Marc-Olivier Maltais <1732415@etu.cegepjonquiere.ca>
2020-10-19 12:20:54 -0400

    essaie num 4 de reglage de conflit

--- Projet1/Main.cpp
+++ Projet1/Main.cpp
@@ -8,7 +8,20 @@ int main()
 	
 	1.Vincent Morin
 	2 - Tanya
-	10 - Marc-OlivierMaltais
+	3 - Maxime
+	4 - Francis
+	5 - Alex
+	6 - Nicolas Occupation Double
+	7 - Jeremie Thiffault
+	8 - Mathieu Bienvenu
+	9 - Matis
+	10-Marc-Olivier Maltais
+	11 - Sulli
+	14.Billy Osmont
+	12 - Fabrice LONTIO
+	13 - Nicolas B-G
+	14 . Billy Osmont 
+	20. Michael PP
 	*/
 	std::string gameName = "MyGame";
 	GameView::Game myGame(800, 600, gameName);
