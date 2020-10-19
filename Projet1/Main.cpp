d16e2d70db78f2d03c342093abe8ffff2a32ca6d
Michael Paré-Pronovost <0833700@etu.cegepjonquiere.ca>
2020-10-19 11:33:34 -0400

    Je push a nouveau mon main

--- Projet1/Main.cpp
+++ Projet1/Main.cpp
@@ -8,7 +8,12 @@ int main()
 	
 	1.Vincent Morin
 	2 - Tanya
+	3 - Maxime
 	4 - Francis
+	6 - Nicolas Occupation Double
+	7 - Jeremie Thiffault
+	14.Billy Osmont
+	20. Michael PP
 	*/
 	std::string gameName = "MyGame";
 	GameView::Game myGame(800, 600, gameName);
