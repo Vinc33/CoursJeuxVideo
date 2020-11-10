3a9b66c11b6652b7e1cc63335efe480666f7b1eb
Dolotboy <maxime-lepage@hotmail.ca>
2020-11-09 22:57:53 -0500

    Création de contenu pour la classe Fire

--- Projet1/GameCode/Game/Objects/Fire.cpp
+++ Projet1/GameCode/Game/Objects/Fire.cpp
@@ -1,1 +1,26 @@
 #include "Fire.h"
+
+Fire::Fire(int hp, bool destroyable)
+{
+	this->sprite = "Assets/Objects/barrel.png";
+	this->hp = hp;
+	this->destroyable = destroyable;
+}
+
+int Fire::verifyHp()
+{
+	if (hp <= 0 && destroyable)
+	{
+		fire.kill(fire);
+	}
+	return hp;
+}
+
+void Fire::checkCollision()
+{
+	if (fire.getCollision(fire))
+	{
+		verifyHp();
+	}
+}
+
--- Projet1/GameCode/Game/Objects/Fire.h
+++ Projet1/GameCode/Game/Objects/Fire.h
@@ -1,5 +1,21 @@
 #pragma once
-class Fire
+#include <String>
+#include "./Entity/BaseEntity.h"
+
+using namespace std;
+
+class Fire : BaseEntity
 {
+private:
+	int hp;
+	bool destroyable;
+	string sprite;
+	BaseEntity fire;
+
+public:
+	Fire::Fire(int hp, bool destroyable);
+	int Fire::verifyHp();
+	void Fire::checkCollision();
+
 };
 
