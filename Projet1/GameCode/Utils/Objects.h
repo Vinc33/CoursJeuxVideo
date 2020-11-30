2dece71badc6a4216e272c2aae97c3d912a50c65
Dolotboy <maxime-lepage@hotmail.ca>
2020-11-30 10:38:47 -0500

    Ajout premier vers player collision #2

---
+++ Projet1/GameCode/Utils/Objects.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "./Game/Objects/Barrel.h"
+#include "./Game/Objects/Container.h"
+#include "./Game/Objects/Crate.h"
+#include "./Game/Objects/Fire.h"
+#include "./Game/Objects/Ladder.h"
+#include "./Game/Objects/Platform.h"
+
